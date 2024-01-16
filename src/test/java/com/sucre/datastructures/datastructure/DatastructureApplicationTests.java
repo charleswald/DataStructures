537e3a8def00513eebcf98c72b9a5a8321c45068
charleswuld <charleswuld@gmail.com>
2024-01-16 15:43:57 +0300

    Initial commit

---
+++ src/test/java/com/sucre/datastructures/datastructure/DatastructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sucre.datastructures.datastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatastructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
